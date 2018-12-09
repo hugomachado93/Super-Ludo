630a795441016f1752cdb6fec0b95180205534e2
Duken64 <gabriel.hering@yahoo.com.br>
2018-12-09 04:03:25 -0200

    Add files via upload

--- src/renderizacao/Desenhos.java
+++ src/renderizacao/Desenhos.java
@@ -5,6 +5,8 @@ import java.awt.Color;
 import java.awt.Graphics2D;
 import java.awt.Stroke;
 
+import logic.Jogador;
+
 public class Desenhos {
 	
 	public Desenhos() {
@@ -20,6 +22,14 @@ public class Desenhos {
 			g2d.setColor(Color.GREEN);
 			g2d.setStroke(new BasicStroke(10));
 			g2d.drawRect(1000, 500, 100, 100);
+		}else if(nJogador == 2) {
+			g2d.setColor(Color.BLUE);
+			g2d.setStroke(new BasicStroke(10));
+			g2d.drawRect(1000, 500, 100, 100);
+		}else {
+			g2d.setColor(Color.YELLOW);
+			g2d.setStroke(new BasicStroke(10));
+			g2d.drawRect(1000, 500, 100, 100);
 		}
 		g2d.setStroke(defaultStroke);
 	}
@@ -29,10 +39,10 @@ public class Desenhos {
 		g2d.setColor(Color.RED);
 		g2d.setStroke(new BasicStroke(2));
 		for(int i=0;i<4;i++) {
+			jogador[0].getPecas().get(i).setBarreira(false);
 			for(int j=0;j<4;j++) {
-				jogador[0].getPecas().get(j).setBarreira(false);
 				if(((casaTemp = jogador[0].getPecas().get(i).getNumCasa()) == jogador[0].getPecas().get(j).getNumCasa()) && (i != j) && casaTemp !=0) {
-					jogador[0].getPecas().get(i).setBarreira(true);
+					jogador[0].getPecas().get(j).setBarreira(true);
 					g2d.drawOval(jogador[0].getPecas().get(j).getX()-2, jogador[0].getPecas().get(j).getY()-2, 45, 45);
 				}
 			}
@@ -40,32 +50,32 @@ public class Desenhos {
 		g2d.setColor(Color.GREEN);
 		g2d.setStroke(new BasicStroke(2));
 		for(int i=0;i<4;i++) {
+			jogador[1].getPecas().get(i).setBarreira(false);
 			for(int j=0;j<4;j++) {
-				jogador[1].getPecas().get(j).setBarreira(false);
 				if(((casaTemp = jogador[1].getPecas().get(i).getNumCasa()) == jogador[1].getPecas().get(j).getNumCasa()) && (i != j) && casaTemp !=0) {
-					jogador[0].getPecas().get(i).setBarreira(true);
+					jogador[1].getPecas().get(j).setBarreira(true);
 					g2d.drawOval(jogador[1].getPecas().get(j).getX()-2, jogador[1].getPecas().get(j).getY()-2, 45, 45);
-				}
-			}
-		}
-		g2d.setColor(Color.YELLOW);
-		g2d.setStroke(new BasicStroke(2));
-		for(int i=0;i<4;i++) {
-			for(int j=0;j<4;j++) {
-				jogador[2].getPecas().get(j).setBarreira(false);
-				if(((casaTemp = jogador[2].getPecas().get(i).getNumCasa()) == jogador[2].getPecas().get(j).getNumCasa()) && (i != j) && casaTemp !=0) {
-					jogador[2].getPecas().get(i).setBarreira(true);
-					g2d.drawOval(jogador[2].getPecas().get(j).getX()-2, jogador[2].getPecas().get(j).getY()-2, 45, 45);
 				}
 			}
 		}
 		g2d.setColor(Color.BLUE);
 		g2d.setStroke(new BasicStroke(2));
 		for(int i=0;i<4;i++) {
+			jogador[2].getPecas().get(i).setBarreira(false);
 			for(int j=0;j<4;j++) {
-				jogador[3].getPecas().get(j).setBarreira(false);
+				if(((casaTemp = jogador[2].getPecas().get(i).getNumCasa()) == jogador[2].getPecas().get(j).getNumCasa()) && (i != j) && casaTemp !=0) {
+					jogador[2].getPecas().get(j).setBarreira(true);
+					g2d.drawOval(jogador[2].getPecas().get(j).getX()-2, jogador[2].getPecas().get(j).getY()-2, 45, 45);
+				}
+			}
+		}
+		g2d.setColor(Color.YELLOW);
+		g2d.setStroke(new BasicStroke(2));
+		for(int i=0;i<4;i++) {
+			jogador[3].getPecas().get(i).setBarreira(false);
+			for(int j=0;j<4;j++) {
 				if(((casaTemp = jogador[3].getPecas().get(i).getNumCasa()) == jogador[3].getPecas().get(j).getNumCasa()) && (i != j) && casaTemp !=0) {
-					jogador[3].getPecas().get(i).setBarreira(true);
+					jogador[3].getPecas().get(j).setBarreira(true);
 					g2d.drawOval(jogador[3].getPecas().get(j).getX()-2, jogador[3].getPecas().get(j).getY()-2, 45, 45);
 				}
 			}
@@ -73,4 +83,36 @@ public class Desenhos {
 		g2d.setStroke(defaultStroke);
 	}
 	
+	public void PecaNaMesmaCasaCoresDiferentes(Jogador[] jogador, Graphics2D g2d, Stroke defaultStroke) {
+
+		g2d.setStroke(new BasicStroke(2));
+		for(int w=0;w<4;w++) {
+			for(int i=0;i<4;i++)
+			{
+				for(int j=0; j<4;j++)
+				{
+					for(int k=0;k<4;k++) {
+						int px = jogador[w].getPecas().get(i).getX();
+						int py = jogador[w].getPecas().get(i).getY();
+						if(jogador[j].getPecas().get(k).getX()==px && jogador[j].getPecas().get(k).getY()==py
+								&& jogador[w] != jogador[j]) {
+							if(j == 0) {
+								g2d.setColor(Color.RED);
+							}else if(j == 1) {
+								g2d.setColor(Color.GREEN);
+							}else if(j == 2) {
+								g2d.setColor(Color.BLUE);
+							}else {
+								g2d.setColor(Color.YELLOW);
+							}
+								g2d.drawOval(jogador[w].getPecas().get(i).getX()-2, jogador[w].getPecas().get(i).getY()-2, 45, 45);
+						}
+					}
+				}
+			}
+		}
+		g2d.setStroke(defaultStroke);
+	}
+	
 }
+
--- src/renderizacao/Game.java
+++ src/renderizacao/Game.java
@@ -11,40 +11,79 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.geom.Ellipse2D;
 import java.awt.geom.Rectangle2D;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Observable;
 import java.util.Observer;
+import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import logic.AtualizacaoLogica;
+import logic.Dado;
+import logic.GameFacade;
+import logic.Jogador;
+
 
 public class Game extends JPanel{
 	private static final long serialVersionUID = 1L;
-	
 	private GameFacade gameFacade = new GameFacade();
 	private Stroke defaultStroke;
+	private Jogador[] jogador;
+	private Desenhos desenhos = new Desenhos();
+	private JButton[] bDebug = new JButton[6];
+	private JButton[] menu = new JButton[3];
+	private int nJogador;
+	private JButton bDice;
+	Dado dado = new Dado();
 	
 	public Game() throws IOException {
+		gameFacade.addObserver(new AtualizacaoLogica());
+		bDice = new JButton("DADO");
+		jogador = gameFacade.getJogador();
+		nJogador = gameFacade.getNumJogador();
+		bDebug[0] = new JButton("1");
+		bDebug[1] = new JButton("2");
+		bDebug[2] = new JButton("3");
+		bDebug[3] = new JButton("4");
+		bDebug[4] = new JButton("5");
+		bDebug[5] = new JButton("6");
+	
+		menu[0] = new JButton("Novo jogo");
+		menu[1] = new JButton("Salvar");
+		menu[2] = new JButton("Carregar");
+		
 		GUI();
 	}
 	
-	private void GUI() {
+	private void GUI() throws IOException {
 		setLayout(null);
-		add(gameFacade.eventDado());
+		bDebug = eventDebugDado();
+		menu = eventMenu();
+		add(eventDado());
 		
-		gameFacade.addObserver((obj, arg)-> {
-			System.out.println("Repainting");
-			repaint();
-		});
+		for(int i=0;i<6;i++) {
+			add(bDebug[i]);
+		}
+		
+		for(int i=0;i<3;i++) {
+			add(menu[i]);
+		}
 		
 		addMouseListener(new MouseAdapter() {
 			@Override
 			public void mousePressed(MouseEvent e) {
+				System.out.println("teste");
 				gameFacade.mouseClicked(e);
+				System.out.println("passou");
+				repaint();
+				gameFacade.jogoAcabou();
+				
 			}
 		});
+		
 	}
 	
 	@Override
@@ -52,8 +91,197 @@ public class Game extends JPanel{
 		super.paintComponent(g);
 		Graphics2D g2d = (Graphics2D) g;
 		defaultStroke = g2d.getStroke();
-		
-		gameFacade.DrawAll(g2d, defaultStroke);
-		
+		DesenhaTodasAsPecas(g2d);
+		DrawAll(g2d, defaultStroke);
+		desenhos.PecaNaMesmaCasaCoresDiferentes(jogador, g2d, defaultStroke);
 	}
+	
+	public void DesenhaTodasAsPecas(Graphics2D g2d) {
+		//printa as pecas
+			for(int i=0;i<4;i++) {
+				g2d.setColor(new Color(150, 0, 0));
+				jogador[0].getPecas().get(i).setEllipse(new Ellipse2D.Double(jogador[0].getPecas().get(i).getX(), jogador[0].getPecas().get(i).getY(), 40, 40));
+				g2d.fill(jogador[0].getPecas().get(i).getEllipse());
+
+			}
+				
+			for(int i=0;i<4;i++) {
+				g2d.setColor(new Color(0, 150, 0));
+				jogador[1].getPecas().get(i).setEllipse(new Ellipse2D.Double(jogador[1].getPecas().get(i).getX(), jogador[1].getPecas().get(i).getY(), 40, 40));
+				g2d.fill(jogador[1].getPecas().get(i).getEllipse());
+			}
+				
+			for(int i=0;i<4;i++) {
+				g2d.setColor(new Color(0, 0, 150));
+				jogador[2].getPecas().get(i).setEllipse(new Ellipse2D.Double(jogador[2].getPecas().get(i).getX(), jogador[2].getPecas().get(i).getY(), 40, 40));
+				g2d.fill(jogador[2].getPecas().get(i).getEllipse());
+			}
+				
+			for(int i=0;i<4;i++) {
+				g2d.setColor(new Color(150, 150, 0));
+				jogador[3].getPecas().get(i).setEllipse(new Ellipse2D.Double(jogador[3].getPecas().get(i).getX(), jogador[3].getPecas().get(i).getY(), 40, 40));
+				g2d.fill(jogador[3].getPecas().get(i).getEllipse());
+			}
+	}
+	
+	public void DrawAll(Graphics2D g2d, Stroke defaultStroke) {
+		Tabuleiro.getTabuleiro().paintTabuleiro(g2d);
+		DrawDado(g2d);
+		DesenhaTodasAsPecas(g2d);
+		DiceColor(g2d, defaultStroke);
+		PecaNaMesmaCasa(g2d, defaultStroke);
+	}
+	
+	public void DiceColor(Graphics2D g2d, Stroke defaultStroke) {
+		desenhos.diceColor(g2d,gameFacade.getNumJogador(), defaultStroke);
+	}
+	
+	public void PecaNaMesmaCasa(Graphics2D g2d, Stroke defaultStroke) {
+		desenhos.PecaNaMesmaCasa(jogador, g2d, defaultStroke);
+	}
+	
+	public void DrawDado(Graphics2D g2d) {
+		dado.drawDado(g2d);
+	}
+	
+	public JButton[] eventDebugDado() {
+		bDebug[0].setBounds(950, 650, 50, 50);
+		bDebug[0].addMouseListener(new MouseAdapter() {
+			@Override
+			 public void mouseClicked(MouseEvent e) {
+				gameFacade.todasPecasNoIncio();
+				if(!Dado.dadoClicado) {
+					dado.setNumDado(1);
+					repaint();
+				}
+				Dado.dadoClicado = true;
+			 }
+		});
+		
+		bDebug[1].setBounds(1000, 650, 50, 50);
+		bDebug[1].addMouseListener(new MouseAdapter() {
+			@Override
+			 public void mouseClicked(MouseEvent e) {
+				gameFacade.todasPecasNoIncio();
+				if(!Dado.dadoClicado) {
+					dado.setNumDado(2);
+					repaint();
+				}
+				Dado.dadoClicado = true;
+			 }
+		});
+		
+		bDebug[2].setBounds(1050, 650, 50, 50);
+		bDebug[2].addMouseListener(new MouseAdapter() {
+			@Override
+			 public void mouseClicked(MouseEvent e) {
+				gameFacade.todasPecasNoIncio();
+				if(!Dado.dadoClicado) {
+					dado.setNumDado(3);
+					repaint();
+				}
+				Dado.dadoClicado = true;
+			 }
+		});
+		
+		bDebug[3].setBounds(1100, 650, 50, 50);
+		bDebug[3].addMouseListener(new MouseAdapter() {
+			@Override
+			 public void mouseClicked(MouseEvent e) {
+				gameFacade.todasPecasNoIncio();
+				if(!Dado.dadoClicado) {
+					dado.setNumDado(4);
+					repaint();
+				}
+				Dado.dadoClicado = true;
+			 }
+		});
+		
+		bDebug[4].setBounds(1150, 650, 50, 50);
+		bDebug[4].addMouseListener(new MouseAdapter() {
+			@Override
+			 public void mouseClicked(MouseEvent e) {
+				gameFacade.todasPecasNoIncio();
+				if(!Dado.dadoClicado) {
+					dado.setNumDado(5);
+					repaint();
+				}
+				Dado.dadoClicado = true;
+			 }
+		});
+		
+		bDebug[5].setBounds(1200, 650, 50, 50);
+		bDebug[5].addMouseListener(new MouseAdapter() {
+			@Override
+			 public void mouseClicked(MouseEvent e) {
+				gameFacade.todasPecasNoIncio();
+				if(!Dado.dadoClicado) {
+					dado.setNumDado(6);
+					repaint();
+				}
+				Dado.dadoClicado = true;
+			 }
+		});
+		
+	return bDebug;
+	}
+	
+	public JButton eventDado() {
+		bDice.setBounds(1000, 400, 100, 50);
+		bDice.addMouseListener(new MouseAdapter() {
+			@Override
+			 public void mouseClicked(MouseEvent e) {
+				gameFacade.todasPecasNoIncio();
+				if(!Dado.dadoClicado) {
+					dado.getRandNumDado();
+					repaint();
+				}
+				Dado.dadoClicado = true;
+			 }
+		});
+		return bDice;
+	}
+	
+	public JButton[] eventMenu() {
+		menu[0].setBounds(1000, 200, 100, 50);
+		menu[0].addMouseListener(new MouseAdapter() {  // novo jogo
+			@Override
+			 public void mouseClicked(MouseEvent e) {
+				gameFacade.zerarPartida();
+				repaint();
+			 }
+		});
+		
+		menu[1].setBounds(1000, 140, 100, 50);
+		menu[1].addMouseListener(new MouseAdapter() {  //salvar
+			@Override
+			 public void mouseClicked(MouseEvent e) {
+				try {
+					gameFacade.salvamento();
+				} catch (IOException e1) {
+					System.out.println("teste");
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				repaint();
+			 }
+		});
+		
+		menu[2].setBounds(1000, 80, 100, 50);
+		menu[2].addMouseListener(new MouseAdapter() {  //carregar
+			@Override
+			 public void mouseClicked(MouseEvent e) {
+				try {
+					gameFacade.carregamento();
+					
+				} catch (FileNotFoundException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+				repaint();
+			 }
+		});
+		return menu;
+	}
+	
 }
